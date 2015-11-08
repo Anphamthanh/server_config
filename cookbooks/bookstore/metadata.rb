e47f254928b70b1109113574612620965fbb8994
An Pham <an.pham@gatech.edu>
2015-11-08 18:59:58 -0500

    Added bookstore cookbook

---
+++ cookbooks/bookstore/metadata.rb
@@ -0,0 +1,9 @@
+name             'bookstore'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures Bookstore App'
+version          '0.0.1'
+
+depends 'setup'
+depends 'monitoring'
