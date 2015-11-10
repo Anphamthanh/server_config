fa48bccf8b60aa4237edb9e98bddd5701656d9cc
An Pham <an.pham@gatech.edu>
2015-11-09 21:51:10 -0500

    Added passenger to the mix

--- cookbooks/bookstore/recipes/packages.rb
+++ cookbooks/bookstore/recipes/packages.rb
@@ -3,6 +3,7 @@
   'mysql-server'
   'mysql-client'
   'nginx'
+  'nginx-extras'
 ).each do |pkg_to_install|
   package pkg_to_install
 end
@@ -10,3 +11,21 @@ end
 execute 'install mysql' do
   command 'gem install mysql2'
 end
+
+script 'add passenger' do
+  interpreter 'bash'
+  code <<-EOH
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
+    echo 'deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main' >> /etc/apt/sources.list.d/passenger.list
+    chown root: /etc/apt/sources.list.d/passenger.list
+    chmod 600 /etc/apt/sources.list.d/passenger.list
+  EOH
+  user 'root'
+end
+
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+end
+
+package 'passenger'
