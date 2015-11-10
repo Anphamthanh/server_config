9086030c387af494531fc30448bb8853c3fcf1cf
An Pham <an.pham@gatech.edu>
2015-11-09 21:12:35 -0500

    Removed git user

--- cookbooks/bookstore/recipes/git.rb
+++ cookbooks/bookstore/recipes/git.rb
@@ -9,7 +9,6 @@ end
 execute 'setup bare git repo' do
   command 'git init --bare bookstore.git'
   cwd node['git-server']['source-dir']
-  user 'git'
   not_if { ::File.exist? "#{node['git-server']['source-dir']}bookstore.git" }
 end
 
@@ -21,8 +20,6 @@ end
 
 template "#{node['git-server']['source-dir']}hooks/post-receive" do
   source 'post-receive-hook.erb'
-  owner 'git'
-  group 'git'
   mode '0755'
   variables({
     working_dir: node['git-server']['server-dir'],
