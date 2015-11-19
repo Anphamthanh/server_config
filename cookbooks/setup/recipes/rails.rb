b2dd5cd4756e4ccfece9c059f436f03225e229f7
An Pham <An_Pham@ultimatesoftware.com>
2015-11-19 13:51:28 -0500

    Update rails install

--- cookbooks/setup/recipes/rails.rb
+++ cookbooks/setup/recipes/rails.rb
@@ -23,59 +23,79 @@ end
   package pkg_to_install
 end
 
-git node['rails']['rbenv'] do
-  repository 'git://github.com/sstephenson/rbenv.git'
-  reference 'master'
-  action :sync
+execute 'get ruby 2.2.3 source' do
+  command 'wget https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.3.tar.gz'
 end
 
-directory "#{node['rails']['rbenv']}plugins"
-
-git "#{node['rails']['rbenv']}plugins/ruby-build" do
-  repository 'git://github.com/sstephenson/ruby-build.git'
-  reference 'master'
-  action :sync
+execute 'extract ruby source' do
+  command 'tar xzvf ruby-2.2.3.tar.gz'
 end
 
-git "#{node['rails']['rbenv']}plugins/rbenv-vars" do
-  repository 'git://github.com/sstephenson/rbenv-vars.git'
-  reference 'master'
-  action :sync
-end
-
-script 'update bash_profile' do
+script 'install ruby' do
   interpreter 'bash'
   code <<-EOH
-    cd
-    echo 'export PATH="#{node['rails']['rbenv']}bin:$PATH"' >> $HOME/.bash_profile
-    echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile
-    echo 'export PATH="#{node['rails']['rbenv']}plugins/ruby-build/bin:$PATH"' >> $HOME/.bash_profile
+    cd ruby-2.2.3
+    ./configure
+    make
+    make install
   EOH
-  user 'root'
 end
 
-execute 'install ruby 2.2.1' do
-  command "RUBY_CONFIGURE_OPTS=--disable-install-doc #{node['rails']['rbenv']}bin/rbenv install -v 2.2.1"
-end
 
-execute 'set ruby 2.2.1 as default' do
-  command "#{node['rails']['rbenv']}bin/rbenv global 2.2.1"
-end
 
-# execute 'disable gem docs' do
-#   command 'echo "gem: --no-rdoc --no-ri" > /root/.gemrc'
+# git node['rails']['rbenv'] do
+#   repository 'git://github.com/sstephenson/rbenv.git'
+#   reference 'master'
+#   action :sync
 # end
-
-execute 'rehash rbenv shims' do
-  command "#{node['rails']['rbenv']}/bin/rbenv rehash"
-end
-
-execute 'install javascript runtime' do
-  command 'add-apt-repository ppa:chris-lea/node.js; apt-get update; apt-get install -y nodejs'
-end
-
-execute 'install rails' do
-  command 'gem install bundler rails --no-rdoc --no-ri'
-end
+#
+# directory "#{node['rails']['rbenv']}plugins"
+#
+# git "#{node['rails']['rbenv']}plugins/ruby-build" do
+#   repository 'git://github.com/sstephenson/ruby-build.git'
+#   reference 'master'
+#   action :sync
+# end
+#
+# git "#{node['rails']['rbenv']}plugins/rbenv-vars" do
+#   repository 'git://github.com/sstephenson/rbenv-vars.git'
+#   reference 'master'
+#   action :sync
+# end
+#
+# script 'update bash_profile' do
+#   interpreter 'bash'
+#   code <<-EOH
+#     cd
+#     echo 'export PATH="#{node['rails']['rbenv']}bin:$PATH"' >> $HOME/.bash_profile
+#     echo 'eval "$(rbenv init -)"' >> $HOME/.bash_profile
+#     echo 'export PATH="#{node['rails']['rbenv']}plugins/ruby-build/bin:$PATH"' >> $HOME/.bash_profile
+#   EOH
+#   user 'root'
+# end
+#
+# execute 'install ruby 2.2.1' do
+#   command "RUBY_CONFIGURE_OPTS=--disable-install-doc #{node['rails']['rbenv']}bin/rbenv install -v 2.2.1"
+# end
+#
+# execute 'set ruby 2.2.1 as default' do
+#   command "#{node['rails']['rbenv']}bin/rbenv global 2.2.1"
+# end
+#
+# # execute 'disable gem docs' do
+# #   command 'echo "gem: --no-rdoc --no-ri" > /root/.gemrc'
+# # end
+#
+# execute 'rehash rbenv shims' do
+#   command "#{node['rails']['rbenv']}/bin/rbenv rehash"
+# end
+#
+# execute 'install javascript runtime' do
+#   command 'add-apt-repository ppa:chris-lea/node.js; apt-get update; apt-get install -y nodejs'
+# end
+#
+# execute 'install rails' do
+#   command 'gem install bundler rails --no-rdoc --no-ri'
+# end
 
 
