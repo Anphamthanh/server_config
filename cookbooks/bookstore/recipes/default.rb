277d0c8670996f694dc0d36795a3b21b72c83193
An Pham <an.pham@gatech.edu>
2015-11-21 15:24:35 -0500

    Updated bookstore default recipe

--- cookbooks/bookstore/recipes/default.rb
+++ cookbooks/bookstore/recipes/default.rb
@@ -1,7 +1,9 @@
 include_recipe 'setup::rails'
-include_recipe 'bookstore::packages'
+include_recipe 'setup::nginx_passenger'
+include_recipe 'setup::mysql'
+
 include_recipe 'bookstore::configure_nginx'
-include_recipe 'bookstore::mysql'
+include_recipe 'bookstore::bookstore_git'
 
 
 
