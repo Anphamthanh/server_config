9f47cd9e8472db37a8c52ed9f559e85a379222d3
An Pham <an.pham@gatech.edu>
2016-01-17 00:00:40 -0500

    Uncomment default recipe

--- cookbooks/bookstore/recipes/default.rb
+++ cookbooks/bookstore/recipes/default.rb
@@ -4,7 +4,7 @@ include_recipe 'setup::mysql'
 
 include_recipe 'bookstore::configure_nginx'
 # copy secret key file to desired location before configure git
-# include_recipe 'bookstore::bookstore_git'
+include_recipe 'bookstore::bookstore_git'
 
 execute 'disable password authentication' do
   command "sed -i s/'#PasswordAuthentication yes'/'PasswordAuthentication no'/g /etc/ssh/sshd_config"
