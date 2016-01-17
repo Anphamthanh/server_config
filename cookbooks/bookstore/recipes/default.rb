b138da676488f0b91071e208f0427742be7ee022
An Pham <an.pham@gatech.edu>
2016-01-16 23:56:59 -0500

    Disable password authentication

--- cookbooks/bookstore/recipes/default.rb
+++ cookbooks/bookstore/recipes/default.rb
@@ -6,5 +6,7 @@ include_recipe 'bookstore::configure_nginx'
 # copy secret key file to desired location before configure git
 # include_recipe 'bookstore::bookstore_git'
 
-
+execute 'disable password authentication' do
+  command "sed -i s/'#PasswordAuthentication yes'/'PasswordAuthentication no'/g /etc/ssh/sshd_config"
+end
 
