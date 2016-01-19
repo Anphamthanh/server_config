50ca1a0b2e5662dbba8ea730fe5baf2e9112c1d9
An Pham <an.pham@gatech.edu>
2016-01-18 23:30:36 -0500

    Add deployer user before configure git

--- cookbooks/bookstore/recipes/default.rb
+++ cookbooks/bookstore/recipes/default.rb
@@ -1,3 +1,12 @@
+
+
+package 'unzip'
+
+user 'deployer' do
+  home '/home/deployer'
+  shell '/bin/bash'
+end
+
 include_recipe 'setup::rails'
 include_recipe 'setup::nginx_passenger'
 include_recipe 'setup::mysql'
@@ -7,10 +16,3 @@ include_recipe 'bookstore::elastic_search'
 # copy secret key file to desired location before configure git
 include_recipe 'bookstore::bookstore_git'
 include_recipe 'bookstore::configure_ssh'
-
-package 'unzip'
-
-user 'deployer' do
-  home '/home/deployer'
-  shell '/bin/bash'
-end
