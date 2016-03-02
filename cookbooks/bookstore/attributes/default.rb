67873898f8795f263a814f6e25353edad1174529
An Pham <An_Pham@ultimatesoftware.com>
2016-03-02 12:45:54 -0500

    Add es_data_path to default attributes

--- cookbooks/bookstore/attributes/default.rb
+++ cookbooks/bookstore/attributes/default.rb
@@ -1,3 +1,4 @@
 default['git-server']['source-dir'] = '/etc/git-server'
 default['git-server']['server-dir'] = '/var/www/bookstore'
 default['secret_key_file_location'] = '/etc/.keys.yml'
+default['elasticsearch']['data_path'] = '/elasticsearch/data'
