fbdf4d4bbe0458f0bd46d9152195b6d0c2f5e980
An Pham <an.pham@gatech.edu>
2016-04-04 23:31:41 -0400

    Moving bookstore cookbook out into infrastructure source code

--- cookbooks/bookstore/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'bookstore'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures Bookstore App'
-version          '0.0.1'
-
-depends 'setup'
-depends 'monitoring'
