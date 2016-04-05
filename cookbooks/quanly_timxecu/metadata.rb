004f6787d396b6793d9af451751d4ab451245bf0
An Pham <an.pham@gatech.edu>
2016-04-04 23:28:11 -0400

    Moving quanly cookbook out into infrastructure source code

--- cookbooks/quanly_timxecu/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'quanly_timxecu'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Secure production server'
-version          '0.1.0'
-
